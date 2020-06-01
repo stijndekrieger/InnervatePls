b7d34c532fbbb92a881249d5aa8239be5929306b
stijndekrieger <stijndekrieger@gmail.com>
2020-06-01 16:21:58 +0200

    initial code

---
+++ innervatepls.lua
@@ -0,0 +1,19 @@
+innervatepls = { } 
+
+-- function innervatepls:HelloWorld()
+--     message("Hello " .. UnitName("player"))
+-- end
+
+-- function innervatepls:HideGryphons()
+--     MainMenuBarLeftEndCap:Hide()
+--     MainMenuBarRightEndCap:Hide()
+-- end
+
+SLASH_INNERVATEPLS1 = "/innervatepls"
+SLASH_INNERVATEPLS2 = "/addontest1"
+SlashCmdList["INNERVATEPLS"] = function(msg)
+   print("Hello World!")
+end
+
+-- innervatepls:HelloWorld()
+-- innervatepls:HideGryphons()
