78960de6f6c3cfc5baa79416ec4d4edd006aaed8
stijndekrieger <stijndekrieger@gmail.com>
2020-06-03 16:57:53 +0200

    added functionality to both size and move

--- innervatepls.lua
+++ innervatepls.lua
@@ -4,12 +4,11 @@ SLASH_INNERVATEPLS1 = "/innervatepls"
 SLASH_INNERVATEPLS2 = "/ipls"
 SlashCmdList["INNERVATEPLS"] = function(msg)
    print("Chat Command Working")
+   
 end
 
---Need to find a way to both size and move
-function InnervatePls_ButtonFrame_OnMouseDown()
-   InnervatePlsButtonFrame:StartSizing() 
-   -- InnervatePlsButtonFrame:StartMoving()
+function InnervatePls_ButtonFrame_OnMouseDown() 
+   InnervatePlsButtonFrame:StartMoving()
 end
 
 function InnervatePls_ButtonFrame_OnMouseUp()
@@ -20,8 +19,24 @@ function InnervatePls_ButtonFrame_OnUpdate()
    InnervatePlsButtonFrame:SetHeight(InnervatePlsButtonFrame:GetWidth())
 end
 
-function InnervatePls_Button_OnLoad()
-   print("Button loaded")
+function InnervatePls_ButtonFrame_OnLoad()
+   InnervatePlsButtonFrame_Button:SetClampedToScreen(true)
+   print("|cff00ccffInnervatePls loaded - /ipls|r")
+end
+
+function InnervatePls_ButtonFrame_Resize_Button_OnUpdate()
+   InnervatePls_Button_Width_Scale = InnervatePlsButtonFrame:GetWidth() / 75
+   InnervatePls_Button_Height_Scale = InnervatePlsButtonFrame:GetHeight() / 75
+   InnervatePlsButtonFrame_Resize_Button:SetWidth(25 * InnervatePls_Button_Width_Scale)
+   InnervatePlsButtonFrame_Resize_Button:SetHeight(25 * InnervatePls_Button_Height_Scale)
+end
+
+function InnervatePls_ButtonFrame_Resize_Button_OnMouseDown()
+   InnervatePlsButtonFrame:StartSizing()
+end
+
+function InnervatePls_ButtonFrame_Resize_Button_OnMouseUp()
+   InnervatePlsButtonFrame:StopMovingOrSizing()
 end
 
 function InnervatePls_Button_OnUpdate()
