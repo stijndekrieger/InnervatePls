1dc6d9571deb5f5ae58ef5a7d927771f518fbe2e
stijndekrieger <stijndekrieger@gmail.com>
2020-06-03 14:37:43 +0200

    scaling button and a few tests

--- innervatepls.lua
+++ innervatepls.lua
@@ -1,19 +1,37 @@
 innervatepls = { } 
 
--- function innervatepls:HelloWorld()
---     message("Hello " .. UnitName("player"))
--- end
-
--- function innervatepls:HideGryphons()
---     MainMenuBarLeftEndCap:Hide()
---     MainMenuBarRightEndCap:Hide()
--- end
-
 SLASH_INNERVATEPLS1 = "/innervatepls"
-SLASH_INNERVATEPLS2 = "/addontest1"
+SLASH_INNERVATEPLS2 = "/ipls"
 SlashCmdList["INNERVATEPLS"] = function(msg)
-   print("Hello World!")
+   print("Chat Command Working")
 end
 
--- innervatepls:HelloWorld()
--- innervatepls:HideGryphons()
+--Need to find a way to both size and move
+function InnervatePls_ButtonFrame_OnMouseDown()
+   InnervatePlsButtonFrame:StartSizing() 
+   -- InnervatePlsButtonFrame:StartMoving()
+end
+
+function InnervatePls_ButtonFrame_OnMouseUp()
+   InnervatePlsButtonFrame:StopMovingOrSizing()
+end
+
+function InnervatePls_ButtonFrame_OnUpdate()
+   InnervatePlsButtonFrame:SetHeight(InnervatePlsButtonFrame:GetWidth())
+end
+
+function InnervatePls_Button_OnLoad()
+   print("Button loaded")
+end
+
+function InnervatePls_Button_OnUpdate()
+   InnervatePls_Button_Width_Scale = InnervatePlsButtonFrame:GetWidth() / 75
+   InnervatePls_Button_Height_Scale = InnervatePlsButtonFrame:GetHeight() / 75
+   InnervatePlsButtonFrame_Button:SetWidth(35 * InnervatePls_Button_Width_Scale)
+   InnervatePlsButtonFrame_Button:SetHeight(35 * InnervatePls_Button_Height_Scale)
+end
+
+function InnervatePls_Button_OnClick()
+   InnervatePls_Class = UnitClass("player")
+   print("You are a " .. InnervatePls_Class)
+end
